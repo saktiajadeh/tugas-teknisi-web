eed2bc5212b18d6e3cac3916a64f886b59fdbd3b
saktiajadeh <saktiajadeh29@gmail.com>
2022-08-02 15:20:08 +0800

    improve cetak laporan

--- resources/views/tugasteknisi/cetakLaporanPDF.blade.php
+++ resources/views/tugasteknisi/cetakLaporanPDF.blade.php
@@ -1,58 +1,64 @@
+<style>
+    #cetakLaporan {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+        font-size: 13px;
+    }
 
-        <style>
-            #cetakLaporan {
-                font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-                border-collapse: collapse;
-                width: 100%;
-                font-size: 13px;
-            }
+    #cetakLaporan td,
+    #cetakLaporan th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
 
-            #cetakLaporan td, #cetakLaporan th {
-                border: 1px solid #ddd;
-                padding: 8px;
-            }
+    #cetakLaporan tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
 
-            #cetakLaporan tr:nth-child(even){background-color: #f2f2f2;}
+    #cetakLaporan tr:hover {
+        background-color: #ddd;
+    }
 
-            #cetakLaporan tr:hover {background-color: #ddd;}
+    #cetakLaporan th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
+    }
 
-            #cetakLaporan th {
-                padding-top: 12px;
-                padding-bottom: 12px;
-                text-align: left;
-                background-color: #4CAF50;
-                color: white;
-            }
-        </style>
+</style>
 
-    <div style="width: 1px; min-width: 100%;">
-      <table id="cetakLaporan" width="100%" border="0" style="border-bottom: 1px solid #ddd;">
+<div style="width: 1px; min-width: 100%;">
+    <table id="cetakLaporan" width="100%" border="0" style="border-bottom: 1px solid #ddd;">
         <thead align="center">
-          <tr align="center">
-            <td width="80px" style="border: 0;">
-              <img src="{{asset('/img/icon-logo.png')}}" style="width: 80px; height: 80px;object-fit: contain;">
-            </td>
-            <td style="border: 0;">
-              <h1 style="margin: 0;"><u>CV. Sinar Bintang Teknik</u></h1>
-          <p style="margin: 0;">
-            Jl. Gn Bromo VIII/Blok V No. 57 Perumnas Monang Maning
-            <br>
-            Telp. 081 246 381 744
-          </p>
-            </td>
-          </tr>
+            <tr align="center">
+                <td width="80px" style="border: 0;">
+                    <img src="{{ asset('/img/icon-logo.png') }}"
+                        style="width: 80px; height: 80px;object-fit: contain;">
+                </td>
+                <td style="border: 0;">
+                    <h1 style="margin: 0;"><u>CV. Sinar Bintang Teknik</u></h1>
+                    <p style="margin: 0;">
+                        Jl. Gn Bromo VIII/Blok V No. 57 Perumnas Monang Maning
+                        <br>
+                        Telp. 081 246 381 744
+                    </p>
+                </td>
+            </tr>
         </thead>
-      </table>
-        <h4 align="left">Laporan Tugas Teknisi&nbsp;{{ $rangeTanggal }}</h4>
-        <table id="cetakLaporan" width="100%" style="margin-top: 20px;">
-            <thead>
-                @if(count($laporan) <= 0 )
+    </table>
+    <h4 align="left">Laporan Tugas Teknisi&nbsp;{{ $rangeTanggal }}</h4>
+    <table id="cetakLaporan" width="100%" style="margin-top: 20px;">
+        <thead>
+            @if(count($laporan) <= 0 )
                 <tr>
-                  <td style="word-break: break-word; font-weight: bold; text-align: center;">
-                    Tidak ada data yang tersedia
-                  </td>
+                    <td style="word-break: break-word; font-weight: bold; text-align: center;">
+                        Tidak ada data yang tersedia
+                    </td>
                 </tr>
-                @else
+            @else
                 <tr>
                     <td style="word-break: break-word; font-weight: bold;">No</td>
                     <td style="word-break: break-word; font-weight: bold;">Nama Pelanggan</td>
@@ -65,29 +71,31 @@
                     <td style="word-break: break-word; font-weight: bold;">Dimulai pada</td>
                     <td style="word-break: break-word; font-weight: bold;">Selesai pada</td>
                 </tr>
-                @endif
-            </thead>
-            <tbody>
-                @foreach($laporan as $data)
-                    <tr>
-                        <td style="word-break: break-word;">{{ $loop->index + 1 }}</td>
-                        <td style="word-break: break-word;">{{ $data->pelanggan->nama }}</td>
-                        <td style="word-break: break-word;">{{ $data->pelanggan->alamat }}</td>
-                        <td style="word-break: break-word;">{{ $data->pelanggan->no_telp }}</td>
-                        <td style="word-break: break-word;">{{ $data->kategorijasa->nama }}</td>
-                        <td style="word-break: break-word;">{{ $data->karyawan->name ?? '-' }}</td>
-                        <td style="word-break: break-word;">{{ $data->detail }}</td>
-                        <td style="word-break: break-word;">{{ $data->status }}</td>
-                        <td style="word-break: break-word;">{{ "Jam: " . $data->jam_mulai . ", Tanggal: ". $data->tanggal_mulai }}</td>
-                        <td style="word-break: break-word;">
-                          @if($data->status === "finish")
-                          {{ "Jam: " . $data->jam_selesai . ", Tanggal: ". $data->tanggal_selesai }}
-                          @else
-                          -
-                          @endif
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+            @endif
+        </thead>
+        <tbody>
+            @foreach($laporan as $data)
+                <tr>
+                    <td style="word-break: break-word;">{{ $loop->index + 1 }}</td>
+                    <td style="word-break: break-word;">{{ $data->pelanggan->nama }}</td>
+                    <td style="word-break: break-word;">{{ $data->pelanggan->alamat }}</td>
+                    <td style="word-break: break-word;">{{ $data->pelanggan->no_telp }}</td>
+                    <td style="word-break: break-word;">{{ $data->kategorijasa->nama }}</td>
+                    <td style="word-break: break-word;">
+                        {{ $data->karyawan->name ?? '-' }}</td>
+                    <td style="word-break: break-word;">{{ $data->detail }}</td>
+                    <td style="word-break: break-word;">{{ $data->status }}</td>
+                    <td style="word-break: break-word;">
+                        {{ $data->jam_mulai . ", ". $data->tanggal_mulai }}</td>
+                    <td style="word-break: break-word;">
+                        @if($data->status === "finish")
+                            {{ $data->jam_selesai . ", ". $data->tanggal_selesai }}
+                        @else
+                            -
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
